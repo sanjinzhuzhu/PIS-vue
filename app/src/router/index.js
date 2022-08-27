eb3d8db5812520363a7bc83c70b6064287a34cf3
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-08-27 14:52:35 +0800

    pis Vuerouter config

---
+++ app/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes: [
+        {
+            path: "/home",
+            component: () => import("@/views/Home")
+        }
+    ]
+})
